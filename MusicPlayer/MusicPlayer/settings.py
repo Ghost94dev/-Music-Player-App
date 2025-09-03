95ffc7eb86cbdb4b5d7e26d98f83436e28e46307
Devalor <madozin.dev@gmail.com>
2025-09-03 19:46:29 +0000

    NEW CHANGES

--- MusicPlayer/MusicPlayer/settings.py
+++ MusicPlayer/MusicPlayer/settings.py
@@ -118,12 +118,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "Music" / "static"]
-STATIC_ROOT = BASE_DIR, 'staticfiles'
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT= BASE_DIR, 'media'
+MEDIA_ROOT= BASE_DIR / 'media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
