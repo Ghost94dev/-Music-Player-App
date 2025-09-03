0a8531a0fce9e332322bebb422c786767cece10c
Devalor <madozin.dev@gmail.com>
2025-09-03 19:10:31 +0000

    NEW CHANGES

--- MusicPlayer/MusicPlayer/settings.py
+++ MusicPlayer/MusicPlayer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53iujsk*#^nfd&gduc9^f$_s83snxp8n1v8%kxsucpa40_6$&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evlin-music-player.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,11 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "Music" / "static"]
+STATIC_ROOT = BASE_DIR, 'staticfiles'
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT= BASE_DIR, 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
