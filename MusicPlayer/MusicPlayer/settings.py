b4874c10ee19f4d9199e8506a25fc5679df51df4
Devalor <madozin.dev@gmail.com>
2025-09-04 15:26:55 +0000

    settings

--- MusicPlayer/MusicPlayer/settings.py
+++ MusicPlayer/MusicPlayer/settings.py
@@ -71,6 +71,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
+            ],
+            'builtins': [
+                'django.templatetags.static',
             ],
         },
     },
