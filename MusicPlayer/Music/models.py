5c78a1579dab60419284ac5a3116af08e7441ae0
Devalor <madozin.dev@gmail.com>
2025-09-04 19:31:10 +0000

    letgo

--- MusicPlayer/Music/models.py
+++ MusicPlayer/Music/models.py
@@ -23,3 +23,23 @@ class Song(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def secure_image_url(self):
+        """Returns the image URL with HTTPS"""
+        if self.image:
+            url = self.image.url
+            if url.startswith('http://'):
+                return url.replace('http://', 'https://', 1)
+            return url
+        return None
+
+    @property
+    def secure_audio_url(self):
+        """Returns the audio URL with HTTPS"""
+        if self.audio_file:
+            url = self.audio_file.url
+            if url.startswith('http://'):
+                return url.replace('http://', 'https://', 1)
+            return url
+        return None
+
