6c3955f1c3128bbf12ae9deaaa93e510905381fd
Devalor <madozin.dev@gmail.com>
2025-09-04 14:46:52 +0000

    new

--- MusicPlayer/MusicPlayer/wsgi.py
+++ MusicPlayer/MusicPlayer/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MusicPlayer.settings')
 
 application = get_wsgi_application()
+
+# Add this for Render deployment
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    import sys
+
+    # For local development
+    if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+        execute_from_command_line(sys.argv)
